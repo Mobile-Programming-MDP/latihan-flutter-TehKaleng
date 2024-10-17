dba73787556d48254d981d90650bb756de960fea
Syauqi Attallah <msyauqiattallah@gmail.com>
2024-10-17 15:17:32 +0700

    detail_header

---
+++ wisata_candi/lib/detailscreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_candi/detailscreen.dart';
+
+class Detailscreen extends StatelessWidget{
+  const Detailscreen ({super.key});
+
+  @override
+Widget build(BuildContext context ) {
+  return const Scaffold(
+    body: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+           )
+      ],
+    ),
+  );
+}
+}
+
