d1d74b404e21ca4855929cee1b8ca5dfb3ddc218
Syauqi Attallah <msyauqiattallah@gmail.com>
2024-11-09 12:59:40 +0700

    Wisata_candi

--- wisata_candi/lib/profil_screen.dart
+++ wisata_candi/lib/profil_screen.dart
@@ -1,32 +1,34 @@
-
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/widgets/profile_info_item.dart';
 
 class ProfilScreen extends StatefulWidget {
-  const ProfilScreen({super.key});
+  const ProfileScreen({super.key});
 
   @override
-  State<ProfilScreen> createState() => _ProfileScreenState();
+  State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
-class _ProfileScreenState extends State<ProfilScreen> {
+class _ProfileScreenState extends State<ProfileScreen> {
   //TODO 1: Deklarasikan variabel yg dibutuhkan
-  bool isSignedIn = false;
-  String fullName = "M Syauqi Attallah";
-  String userName = "Uqi";
+  bool isSignedIn = true;
+  String fullName = "Nur Rachmat";
+  String username = "nurrachmat";
   int favoriteCandiCount = 0;
 
-//TODO 5: Implemmentasi Fungsi Sign In
-void SignIn () {
-  setState(() {
-    isSignedIn = !isSignedIn;
-  });
-}
-//TODO 6: Implementasi Fungsi Sign Out
-void signOut () {
-  setState(() {
-    isSignedIn = !isSignedIn;
-  });
-}
+  //TODO 5 : Fungsi SignIn
+  void signIn() {
+    setState(() {
+      isSignedIn = !isSignedIn;
+    });
+  }
+
+  //TODO 6 : Fungsi SignOut
+  void signOut() {
+    setState(() {
+      isSignedIn = !isSignedIn;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,133 +43,112 @@ void signOut () {
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               children: [
-                //TODO 2: Buat Bagian Profileheader berisi gambar
+                //TODO 2: Buat bagian ProfileHeader berisi gambar
                 Align(
                   alignment: Alignment.topCenter,
                   child: Padding(
-                    padding: const EdgeInsets.only(top :200 - 50),
+                    padding: const EdgeInsets.only(top: 200 - 50),
                     child: Stack(
-                    alignment: Alignment.bottomRight,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: 
-                            Border.all(color: Colors.deepPurple, width: 2),
-                          shape: BoxShape.circle),
-                        child: CircleAvatar(
-                          radius: 50,
-                          backgroundImage: 
-                          AssetImage("images/placeholder_image.png"),
-                        ) ,
-                      ),
-                      if(isSignedIn)
-                        IconButton(
-                          onPressed:() {},
-                          icon: Icon(
-                            Icons.camera_alt,
-                            color: Colors.deepPurple[50],
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.deepPurple, width: 2),
+                              shape: BoxShape.circle),
+                          child: const CircleAvatar(
+                            radius: 50,
+                            backgroundImage:
+                                AssetImage("images/placeholder_image.png"),
+                          ),
+                        ),
+                        if (isSignedIn)
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.camera_alt,
+                              color: Colors.deepPurple[50],
                             ),
-                      )
-                    ],
+                          )
+                      ],
                     ),
                   ),
                 ),
-                //TODO 3: Buat Bagian ProfileInfo
-                //row username/pengguna
-                SizedBox(height: 20, ),
+                //TODO 3: Buat bagian ProfileInfo
+                //row username /pengguna
+                const SizedBox(height: 20),
                 Divider(color: Colors.deepPurple[100]),
-                SizedBox(height: 4),
-                Row(
-                  //row nama00
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width / 3,
-                      child: Row(
-                        children: [
-                          Icon(Icons.lock, color: Colors.amber),
-                          SizedBox(width: 8),
-                          Text('Pengguna', 
-                          style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                            )
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      child: Text(
-                        ': $userName',
-                        style: TextStyle(fontSize: 18),
-                        ),
-                      ),
-                      if (isSignedIn) Icon(Icons.edit),
-                  ],
+                const SizedBox(height: 4),
+                ProfileInfoItem(
+                  icon: Icons.lock,
+                  label: "Pengguna",
+                  value: username,
+                  iconColor: Colors.amber,
                 ),
-                SizedBox(height: 20, ),
+                //row nama
+                const SizedBox(height: 4),
                 Divider(color: Colors.deepPurple[100]),
-                SizedBox(height: 4),
+                const SizedBox(height: 4),
                 Row(
-                  //row nama00
                   children: [
                     SizedBox(
                       width: MediaQuery.of(context).size.width / 3,
-                      child: Row(
+                      child: const Row(
                         children: [
                           Icon(Icons.person, color: Colors.blue),
                           SizedBox(width: 8),
-                          Text('Nama', 
-                          style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                            )
+                          Text(
+                            'Nama',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          )
                         ],
                       ),
                     ),
                     Expanded(
                       child: Text(
                         ': $fullName',
-                        style: TextStyle(fontSize: 18),
-                        ),
+                        style: const TextStyle(fontSize: 18),
                       ),
-                      if (isSignedIn) Icon(Icons.edit),
+                    ),
+                    if (isSignedIn) const Icon(Icons.edit),
                   ],
                 ),
-                SizedBox(height: 20, ),
+                //row favorit
+                const SizedBox(height: 4),
                 Divider(color: Colors.deepPurple[100]),
-                SizedBox(height: 4),
+                const SizedBox(height: 4),
                 Row(
-                  //row nama00
                   children: [
                     SizedBox(
                       width: MediaQuery.of(context).size.width / 3,
-                      child: Row(
+                      child: const Row(
                         children: [
                           Icon(Icons.favorite, color: Colors.red),
                           SizedBox(width: 8),
-                          Text('Favorit', 
-                          style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                            )
+                          Text(
+                            'Favorit',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          )
                         ],
                       ),
                     ),
                     Expanded(
                       child: Text(
                         ': $favoriteCandiCount',
-                        style: TextStyle(fontSize: 18),
-                        ),
+                        style: const TextStyle(fontSize: 18),
                       ),
-                      if (isSignedIn) Icon(Icons.edit),
+                    ),
                   ],
                 ),
-                SizedBox(height: 20, ),
-                Divider(color: Colors.deepPurple[100]),
-                SizedBox(height: 4),
                 //TODO 4: Buat bagian profile action
-                isSignedIn ? TextButton(
-                  onPressed: signOut,
-                  child: Text('Sign Out'))
-                  :TextButton(
-                    onPressed: SignIn,
-                    child: Text('Sign In')),
+                const SizedBox(height: 4),
+                Divider(color: Colors.deepPurple[100]),
+                const SizedBox(height: 20),
+                isSignedIn
+                    ? TextButton(onPressed: signOut, child: const Text('Sign Out'))
+                    : TextButton(onPressed: signIn, child: const Text('Sign In'))
               ],
             ),
           )
